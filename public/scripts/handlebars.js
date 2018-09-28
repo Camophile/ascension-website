26c94f7fde439f05797bb58115de5509748b54c9
Arman Kazemi <kas1er@hotmail.com>
2018-09-28 13:54:43 -0700

    starting adding templates and partials functionality

--- public/scripts/handlebars.js
+++ public/scripts/handlebars.js
@@ -1,9 +1,25 @@
+// Expression helper
+Handlebars.registerHelper('testHelper', function(property) {
+  return 'this does something';
+});
+
+// Block helper
+Handlebars.registerHelper('blockHelper', function(options) {
+  return options.fn(this); // fn = everything that
+                          // appears in opening and closing block
+});
+
+Handlebars.registerPartial('header',
+  $('#header-partial').html());
 
 // Compile the template data into a function
 jQuery(document).ready(function( $ ) {
 
   var homeTemplate = Handlebars.templates.main({title: 'Ascension'});
   var aboutTemplate = Handlebars.templates.about({title: 'About Us'});
+  // var homeTemplate = $("hbs-template").html();
+  // var compiledHomeTemplate = Handlebars.compile(homeTemplate);
+  // $('#home').html(compiledHomeTemplate({title: 'Ascension'}));
   $('#home').html(homeTemplate);
   $('#about-page').html(aboutTemplate);
 });
