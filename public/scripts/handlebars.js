326d42de7c51d7d1c86a4ab0db99e2dde46edc26
Arman Kazemi <kas1er@hotmail.com>
2018-09-28 15:49:37 -0700

    passing in partials as templates then changing them to partials loading document

--- public/scripts/handlebars.js
+++ public/scripts/handlebars.js
@@ -1,25 +1,40 @@
-// Expression helper
+/* // Expression helper
 Handlebars.registerHelper('testHelper', function(property) {
   return 'this does something';
-});
+}); */
 
-// Block helper
+/* // Block helper
 Handlebars.registerHelper('blockHelper', function(options) {
   return options.fn(this); // fn = everything that
                           // appears in opening and closing block
-});
+}); */
 
+/* // Partials
 Handlebars.registerPartial('header',
   $('#header-partial').html());
 
+  */
+
 // Compile the template data into a function
 jQuery(document).ready(function( $ ) {
+  try {
 
-  var homeTemplate = Handlebars.templates.main({title: 'Ascension'});
-  var aboutTemplate = Handlebars.templates.about({title: 'About Us'});
-  // var homeTemplate = $("hbs-template").html();
-  // var compiledHomeTemplate = Handlebars.compile(homeTemplate);
-  // $('#home').html(compiledHomeTemplate({title: 'Ascension'}));
-  $('#home').html(homeTemplate);
-  $('#about-page').html(aboutTemplate);
+    // $.ajax('../../views/partials/header.html')
+      // .done(function(headerPartial) {
+        // $("body").append(headerPartial);
+        // Handlebars.registerPartial('header', $('#header-partial').html());
+        var homeTemplate = Handlebars.templates.main({title: 'Ascension'});
+        var aboutTemplate = Handlebars.templates.about({title: 'About Us'});
+        var headerTemplate = Handlebars.templates.header();
+        var footerTemplate = Handlebars.templates.footer();
+        Handlebars.partials = Handlebars.templates;
+        $('#home').html(homeTemplate);
+        $('#header').html(headerTemplate);
+        $('#footer').html(footerTemplate);
+      // })
+
+  //$('#about-page').html(aboutTemplate);
+  } catch (e) {
+    console.error(e.stack);
+  }
 });
