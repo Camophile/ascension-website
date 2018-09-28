04c35169ead40d1fbbd9be3a52a07adfee39a26f
Arman Kazemi <kas1er@hotmail.com>
2018-09-28 10:26:56 -0700

    added about route with new about.html in root and handlebars.js script. Need to abstract out partials

--- public/scripts/handlebars.js
+++ public/scripts/handlebars.js
@@ -1,21 +1,9 @@
 
 // Compile the template data into a function
 jQuery(document).ready(function( $ ) {
-  var templateScript;
-  var layouts = $('#layouts');
-  templateScript = Handlebars.templates.main({title: 'Ascension'});
-  layouts.html(templateScript);
-  // $.ajax({
-  //   type: "GET",
-  //   crossDomain: true,
-  //   url: "/views/home/index.html",
-  //   dataType: "html",
-  //   success: function(body) {
-  //     templateScript = Handlebars.templates.main({body});
-  //     layouts.html(templateScript);
-  //   },
-  //   error: function (data) {
-  //     console.error(data);
-  //   }
-  // });
+
+  var homeTemplate = Handlebars.templates.main({title: 'Ascension'});
+  var aboutTemplate = Handlebars.templates.about({title: 'About Us'});
+  $('#home').html(homeTemplate);
+  $('#about-page').html(aboutTemplate);
 });
